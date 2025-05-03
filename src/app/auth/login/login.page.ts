415bfc715a37aa3ba99c2c793f859cd0bf77abfa
0l3x <olexanderg3@gmail.com>
2025-05-03 19:27:46 +0200

    login basico funcional

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-login',
@@ -17,28 +18,45 @@ import { IonicModule } from '@ionic/angular';
   ]
 })
 export class LoginPage {
-  loginForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+  loginForm: FormGroup = this.fb.group({
+    login: ['', [Validators.required, Validators.email]], // CAMBIO
+    password: ['', Validators.required]
+  });
+
+  login() {
+    const data = this.loginForm.value;
+    console.log('Datos enviados:', data);
+  
+    this.authService.login(data).subscribe({
+      next: async (rol) => {
+        console.log('Login correcto');
+        console.log('Rol recibido directamente de la API:', rol);
+  
+        // Recuperar el token desde Preferences (no localStorage)
+        const { value: token } = await Preferences.get({ key: 'fs-token' });
+  
+        console.log('Token guardado:', token);
+  
+        if (token) {
+          const role = this.authService.decodeToken(token).role;
+          console.log('Rol decodificado:', role);
+  
+          if (role === 'admin') {
+            this.router.navigate(['/patient/list']);
+          } else {
+            this.router.navigate(['/appointment/list']);
+          }
+        }
+      },
+      error: (err: any) => {
+        console.error('Error en login:', err);
+        alert('Login incorrecto. Verifica tus datos.');
+      }
     });
   }
-
-  async onLogin() {
-    const { email, password } = this.loginForm.value;
-    const token = await this.authService.login(email, password);
-    if (token) {
-      const role = this.authService.decodeToken(token).role;
-
-      // Navegación según el rol
-      if (role === 'admin') this.router.navigate(['/patient/list']);
-      else this.router.navigate(['/appointment/list']);
-    }
-  }
+  
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,31 +1,63 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Preferences } from '@capacitor/preferences';
+import { catchError, from, map, Observable, of, switchMap } from 'rxjs';
+import { UserLogin } from '../interfaces/user';
+import { TokenResponse } from '../interfaces/responses';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private token: string = '';
+  #logged = signal(false);
 
-  constructor(private http: HttpClient) {}
+  #http = inject(HttpClient);
+  
+  getLogged() {
+    return this.#logged.asReadonly();
+  }  
 
-  async login(email: string, password: string): Promise<string | null> {
-    try {
-      const res: any = await this.http.post('https://hectorrp.com/api/physios', { email, password }).toPromise();
-      this.token = res.token;
-      await Preferences.set({ key: 'token', value: this.token });
-      return this.token;
-    } catch (err) {
-      console.error('Login error', err);
-      return null;
+  login (data: UserLogin): Observable<void> {
+    return this.#http.post<TokenResponse>(`auth/login`, data).pipe(
+      // SwitchMap permite trabajar con funciones que devuelven observables o promesas
+      switchMap(async (res) => { // Función async, devuelve promesa (Promise<void>)
+        try {
+          await Preferences.set({ key: 'fs-token', value: res.token });
+          this.#logged.set(true);
+        } catch (e) {
+          throw new Error('Can\'t save authentication token in storage!');
+        }
+      })
+    );
+  }  
+
+      decodeToken(token: string): any {
+        return JSON.parse(atob(token.split('.')[1]));
+      }  
+
+  async logout(): Promise<void> {
+    await Preferences.remove({ key: 'fs-token' });
+    this.#logged.set(false);
+  }
+
+  isLogged(): Observable<boolean> {
+    if (this.#logged()) { // Estamos logueados
+      return of(true);
     }
+    // from transforma una promesa en un observable
+    return from(Preferences.get({ key: 'fs-token' })).pipe(
+      switchMap((token) => {
+        if (!token.value) { // No hay token
+          return of(false);
+        }
+
+        return this.#http.get('auth/validate').pipe(
+          map(() => {
+            this.#logged.set(true);
+            return true; // Todo correcto
+          }),
+          catchError(() => of(false)) // Token no válido
+        );
+      }),
+    );
   }
 
-  async getToken(): Promise<string | null> {
-    const { value } = await Preferences.get({ key: 'token' });
-    return value || null;
-  }
-
-  decodeToken(token: string): any {
-    return JSON.parse(atob(token.split('.')[1]));
-  }
 }
