4d104086aadd65fdca897c6ea38cd6d736b4aeb7
0l3x <olexanderg3@gmail.com>
2025-05-29 00:46:08 +0200

    mapa y gps implementado

--- src/app/profile/profile/profile-location/profile-location.page.ts
+++ src/app/profile/profile/profile-location/profile-location.page.ts
@@ -1,17 +1,18 @@
 import { afterNextRender, Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { ProfilePage } from '../profile.page';
 import { OlMapDirective } from 'src/app/shared/ol-maps/ol-map.directive';
 import { OlMarkerDirective } from 'src/app/shared/ol-maps/ol-marker.directive';
+import { LaunchNavigator } from '@awesome-cordova-plugins/launch-navigator';
 
 @Component({
   selector: 'profile-location',
   templateUrl: './profile-location.page.html',
   styleUrls: ['./profile-location.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, OlMapDirective, OlMarkerDirective]
+  imports: [IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, OlMapDirective, OlMarkerDirective, IonFab, IonFabButton, IonIcon]
 })
 export class ProfileLocationPage {
   patient = inject(ProfilePage).patient;
@@ -26,6 +27,10 @@ export class ProfileLocationPage {
           ])
         }
       })
+   }
+
+   openGPS() {
+      LaunchNavigator.navigate(this.coordinates().reverse());
    }
 
 }
