fd3e46b9a34dde7704532b20327ab00f0e974f0e
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-05-28 15:38:45 +0200

    Update things

--- src/app/patient/interfaces/patient.ts
+++ src/app/patient/interfaces/patient.ts
@@ -1,13 +1,13 @@
 export interface PatientInsert {
-    name?: string;
-    surname?: string;
-    birthDate?: string;
-    address?: string;
-    insuranceNumber?: string;
-    email?: string;
-    userID?: string;
-    avatar?: string;
+  name?: string;
+  surname?: string;
+  birthDate?: string;
+  address?: string;
+  insuranceNumber?: string;
+  email?: string;
+  userID?: string;
+  avatar?: string;
 }
 export interface Patient extends PatientInsert {
-    _id: string;
+  _id: string;
 }
