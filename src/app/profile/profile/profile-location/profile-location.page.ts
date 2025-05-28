8844b41e048a9a3b063235ab4b6461d9137de56a
0l3x <olexanderg3@gmail.com>
2025-05-29 00:32:17 +0200

    location tab agregado
    location tab agregado

--- src/app/profile/profile/profile-location/profile-location.page.ts
+++ src/app/profile/profile/profile-location/profile-location.page.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { afterNextRender, Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ProfilePage } from '../profile.page';
+import { OlMapDirective } from 'src/app/shared/ol-maps/ol-map.directive';
+import { OlMarkerDirective } from 'src/app/shared/ol-maps/ol-marker.directive';
 
 @Component({
   selector: 'profile-location',
   templateUrl: './profile-location.page.html',
   styleUrls: ['./profile-location.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, OlMapDirective, OlMarkerDirective]
 })
-export class ProfileLocationPage implements OnInit {
+export class ProfileLocationPage {
+  patient = inject(ProfilePage).patient;
+  coordinates = signal<[number, number]>([0, 0]);
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor() { 
+      afterNextRender(() => {
+        if(this.patient()){
+          this.coordinates.set([
+            this.patient()!.lng ?? 0,
+            this.patient()!.lat ?? 0
+          ])
+        }
+      })
+   }
 
 }
